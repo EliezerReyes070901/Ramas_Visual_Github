44a1b775c8e73205de9d25bd8dd423f9c9efdd66
Eliezer Reyes <Eliezer Reyes@DESKTOP-7918O69>
2024-11-15 10:26:52 -0600

    Agregar archivos de proyecto.

---
+++ Ramas en VisualStudio-Github/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ramas_en_VisualStudio_Github.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
